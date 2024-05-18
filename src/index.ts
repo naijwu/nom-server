2d846446916f514e9afca335369177df417b4a6a
martincai8 <martincai46@gmail.com>
2024-05-18 13:21:41 -0700

    Specify PORT

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import googlePlacesRoutes from './routes/googlePlacesRoutes';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, world!');
